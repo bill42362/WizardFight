a4bacb555c02a6a2518f3f1345faf09db61a8a7c
bill42362 <bill42362@gmail.com>
2015-10-06 00:52:04 +0800

    Add prototype of PlayerMoveTarget.

--- Assets/WizardFightController.js
+++ Assets/WizardFightController.js
@@ -3,6 +3,8 @@ private var app: WizardFightApplication; // WizardFightApplication.js
 private var components: WizardFightComponents; // WizardFightComponents.js
 private var model: WizardFightModel; // WizardFightModel.js
 private var view: WizardFightView; // WizardFightView.js
+private var groundPlane = Plane(Vector3(0.0, 1.0, 0.0), Vector3(0, 0, 0));
+private var playerTargetPosition: Vector3 = Vector3(0, 0.5, -5);
 
 function Start () {
 	app = WizardFightApplication.Shared();
@@ -24,7 +26,7 @@ function Update () {
 	}
 	if(null == model.playerModel) {
 		var playerModel = Instantiate(components.WizardModel);
-		playerModel.gameObject.transform.position = Vector3(0, 0, -5);
+		playerModel.gameObject.transform.position = Vector3(0, 0.5, -5);
 		model.playerModel = playerModel;
 		playerModel.gameObject.name = 'player';
 		playerModel.gameObject.SetActive(true);
@@ -32,5 +34,18 @@ function Update () {
 		playerView.wizardModel = playerModel;
 		view.playerView = playerView;
 		playerView.gameObject.SetActive(true);
+	}
+	if(Input.GetMouseButtonUp(0)) {
+		var ray: Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var rayDistance: float;
+		if(groundPlane.Raycast(ray, rayDistance)) {
+			playerTargetPosition = ray.GetPoint(rayDistance);
+			playerTargetPosition.y = 0.5;
+		}
+	}
+	if(null != playerTargetPosition) {
+		var targetDirection = playerTargetPosition - model.playerModel.gameObject.transform.position;
+		var force = targetDirection*15;
+		model.playerModel.GetComponent.<Rigidbody>().velocity = force;
 	}
 }
