22d81b3731fc56641651992c6ec7fcd87db29c86
simpdanny <simpdanny@gmail.com>
2015-10-27 19:30:06 +0800

    connect button

---
+++ Assets/Photon Unity Networking/Resources/NetworkHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkHandler : Photon.PunBehaviour
+{
+
+    public void connectToPhoton()
+    {
+        if ( !PhotonNetwork.connected )
+            PhotonNetwork.ConnectUsingSettings("0.001");
+        else
+        {
+            Debug.Log("Connection Duplicated!!!");
+        }
+    }
+    public override void OnConnectedToPhoton()
+    {
+        base.OnConnectedToPhoton();
+        Button button = this.GetComponent<Button>();
+        button.interactable = false;
+        Debug.Log("Connection Success!!!");
+    }
+}
