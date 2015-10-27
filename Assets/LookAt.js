0629147d63283cbdb9a3a6953bdcefffa9cf9649
Pi-Yin Hsiao <bill42362@gmail.com>
2015-10-27 22:08:56 +0800

    Get LookAt target by 'Enemy' tag.

--- Assets/LookAt.js
+++ Assets/LookAt.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 var target: GameObject;
 var up: Vector3 = new Vector3(0, 1, 0);
+function Awake() { target = FindWithTag('Enemy'); }
 function Update () {
 	if(null != target) { transform.LookAt(target.transform, up); }
 }
