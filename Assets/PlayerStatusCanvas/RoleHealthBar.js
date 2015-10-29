708326227e6bccae454a7354005bfca9216f8e12
bill42362 <bill42362@gmail.com>
2015-10-30 01:00:32 +0800

    Modify role getting method to event triggered style.

--- Assets/PlayerStatusCanvas/RoleHealthBar.js
+++ Assets/PlayerStatusCanvas/RoleHealthBar.js
@@ -1,12 +1,19 @@
 ﻿#pragma strict
-var roleGameObject: GameObject;
+var role: Role;
+private var eventCenter: EventCenter;
 private var slider: Slider;
 
-function Awake () { slider = GetComponent(Slider); }
+function Awake () {
+	eventCenter = GameObject.FindWithTag('EventCenter').GetComponent(EventCenter);
+	eventCenter.RegisterListener(eventCenter, 'playerChanged', gameObject, OnPlayerChanged);
+	slider = GetComponent(Slider);
+}
 function Update () {
-	roleGameObject = GameObject.FindWithTag('Player');
-	if(null != roleGameObject) {
-		var role: Role = roleGameObject.GetComponent(Role);
+	if(null != role) {
 		slider.value = role.health/role.maxHealth;
 	}
+}
+function OnPlayerChanged(e: SbiEvent) {
+	var roleGameObject = e.data as GameObject;
+	role = roleGameObject.GetComponent(Role);
 }
