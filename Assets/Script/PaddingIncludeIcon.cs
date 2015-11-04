e774a5215f80c435c93c4cc2c4c97279d8a41764
Bill42362 <bill42362@gmail.com>
2015-11-04 15:06:43 +0800

    Modify layout of bars and put them into new folder.

---
+++ Assets/Script/PaddingIncludeIcon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PaddingIncludeIcon : MonoBehaviour {
+	public float padding = 5f;
+	public bool skipIcon = true;
+	public void Awake () {
+		Rect parentRect = transform.parent.GetComponent<RectTransform>().rect;
+		float width = parentRect.width - 2f*padding;
+		if(true == skipIcon) {
+			width -= parentRect.height + padding;
+		}
+		GetComponent<RectTransform>().sizeDelta = new Vector2(width, 1f);
+	}
+}
