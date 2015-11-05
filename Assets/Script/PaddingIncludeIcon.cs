b301b9dfb1c0cd219c91c65ccbbe805b2f378739
Bill42362 <bill42362@gmail.com>
2015-11-05 15:27:40 +0800

    Fix zero-width-bugs for PaddingIncludeIcon.

--- Assets/Script/PaddingIncludeIcon.cs
+++ Assets/Script/PaddingIncludeIcon.cs
@@ -4,12 +4,25 @@ using UnityEngine.UI;
 public class PaddingIncludeIcon : MonoBehaviour {
 	public float padding = 5f;
 	public bool skipIcon = true;
+	private bool layoutFinished = false;
+	private RectTransform parentRectTransform;
 	public void Awake () {
-		Rect parentRect = transform.parent.GetComponent<RectTransform>().rect;
-		float width = parentRect.width - 2f*padding;
+		parentRectTransform= transform.parent.GetComponent<RectTransform>();
+		if(0 != parentRectTransform.rect.width) {
+			Layout();
+		}
+	}
+	public void Update () {
+		if((false == layoutFinished) && (0 != parentRectTransform.rect.width)) {
+			Layout();
+		}
+	}
+	private void Layout() {
+		float width = parentRectTransform.rect.width - 2f*padding;
 		if(true == skipIcon) {
-			width -= parentRect.height + padding;
+			width -= parentRectTransform.rect.height + padding;
 		}
 		GetComponent<RectTransform>().sizeDelta = new Vector2(width, 1f);
+		layoutFinished = true;
 	}
 }
