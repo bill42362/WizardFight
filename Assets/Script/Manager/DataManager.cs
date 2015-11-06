48642f4185131954a45a542f513378cc38f3d494
unknown <>
2015-11-06 20:11:28 +0800

    DataManager

---
+++ Assets/Script/Manager/DataManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager : MonoBehaviour {
+    private static DataManager _instance;
+    protected DataManager()
+    {
+    }
+    public static DataManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Object.FindObjectOfType(typeof(DataManager)) as DataManager;
+                if (_instance == null)
+                {
+                    GameObject gm = new GameObject("DataManager");
+                    DontDestroyOnLoad(gm);
+                    _instance = gm.AddComponent<DataManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
