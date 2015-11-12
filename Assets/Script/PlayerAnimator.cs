9d72eccfa0343af74867e80297d1166b12083b53
Bill42362 <bill42362@gmail.com>
2015-11-12 18:00:09 +0800

    Modify transition conditions of PlayerAnimator.

--- Assets/Script/PlayerAnimator.cs
+++ Assets/Script/PlayerAnimator.cs
@@ -2,16 +2,15 @@
 using System.Collections;
 
 public class PlayerAnimator : MonoBehaviour {
-
     public Animator animator;
-	// Use this for initialization
-	void Start () {
-		animator = gameObject.GetComponent<Animator> ();
-	}
+	public float velocityX;
+	private Rigidbody rigidbody;
 	
-	// Update is called once per frame
+	void Start () {
+		animator = GetComponent<Animator>();
+		rigidbody = GetComponent<Rigidbody>();
+	}
 	void Update () {
-		// 把角色的速度值傳給 animator 中的 velocity
-        animator.SetFloat ("velocity", this.GetComponent<Rigidbody>().velocity.magnitude);
+        animator.SetFloat("velocityX", rigidbody.velocity.x);
 	}
 }
