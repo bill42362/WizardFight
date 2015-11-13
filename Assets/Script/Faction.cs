f11b45b17ca9427938263c9f73cec918ac99a64d
unknown <>
2015-11-13 20:52:42 +0800

    Refactor Faction.cs

--- Assets/Script/Faction.cs
+++ Assets/Script/Faction.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class Faction : Photon.PunBehaviour {
 
-    public int owner = 1; // 0 = neutral;
+    public int owner = -1; // -1 == unset , 0 == neutral;
     // Use this for initialization
     void Start () {
-	}
+        if (owner == -1)
+            SetNeutral();
+    }
 	// Update is called once per frame
 	void Update () {
-	
 	}
     public void SetNeutral()
     {
@@ -18,11 +19,12 @@ public class Faction : Photon.PunBehaviour {
     public override void OnPhotonInstantiate(PhotonMessageInfo info)
     {
         base.OnPhotonInstantiate(info);
-        if ( !NetworkManager.Instance.isOffline )
+        if ( owner == -1 && !NetworkManager.Instance.isOffline )
         {
-            owner = info.sender.ID;
+            SetFaction(info.sender.ID);
         }
     }
+    
     public bool IsRival(Faction faction)
     {
         return ( faction == null || owner != faction.owner );
@@ -30,5 +32,9 @@ public class Faction : Photon.PunBehaviour {
     public int GetFaction()
     {
         return owner;
+    }
+    public void SetFaction( int ownerid )
+    {
+        this.owner = ownerid;
     }
 }
