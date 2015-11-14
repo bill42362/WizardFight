95294bd59718e6bfc08802331c985df160e8805c
Bill42362 <bill42362@gmail.com>
2015-11-14 22:17:21 +0800

    Use rpc to sync role move.

--- Assets/Script/RoleEventController.cs
+++ Assets/Script/RoleEventController.cs
@@ -13,13 +13,17 @@ public class RoleEventController : MonoBehaviour {
 		eventPairs["rightButtonClick"] = new Vector3(1, 0, 0);
 		var e = eventPairs.GetEnumerator();
 		while(e.MoveNext()) {
-			EventManager.Instance.RegisterListener(EventManager.Instance, e.Current.Key, gameObject, OnEventTriggered);
+			EventManager.Instance.RegisterListener(EventManager.Instance, e.Current.Key, gameObject, OnEventTriggeredRPC);
 		}
 	}
-	public void OnEventTriggered(SbiEvent e) {
+	public void OnEventTriggeredRPC(SbiEvent e) {
+		GetComponent<PhotonView>().RPC("OnEventTriggered", PhotonTargets.AllBufferedViaServer, e.type);	
+	}
+	[PunRPC]
+	public void OnEventTriggered(string type) {
 		float speed = 10;
 		if(null != role) { speed = (float)role.speed; }
-		Vector3 velocity = transform.localToWorldMatrix.MultiplyVector(eventPairs[e.type]*speed);
+		Vector3 velocity = transform.localToWorldMatrix.MultiplyVector(eventPairs[type]*speed);
 		if(null != rigidbody) { rigidbody.velocity = velocity; }
 	}
 }
