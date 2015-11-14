cb00845a5064fd50015f39d803a559182e5661a2
unknown <>
2015-11-14 21:38:53 +0800

    Init skillhandler

---
+++ Assets/Script/Skill/SkillHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillHandler : Photon.PunBehaviour{
+    Hashtable skillCasterTable = new Hashtable();
+    void Awake()
+    {
+        EventManager.Instance.RegisterListener(GameManager.Instance, "playerSkillsReady", this.gameObject, OnPlayerSkillsReady);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnPlayerSkillsReady(SbiEvent e)
+    {
+        PlayerSkillsReadyEventData data = (PlayerSkillsReadyEventData)e.data;
+        for ( int i = 0; i < data.skillIDs.Length; i++ )
+        {
+            GameObject obj = data.skillCasters[i];
+            skillCasterTable[i] = obj;
+            obj.transform.parent = this.gameObject.transform;
+        }
+
+    }
+
+    [PunRPC]
+    void StartCasting( int skillID) {
+    }
+    [PunRPC]
+    void StopCasting( int skillID )
+    {
+   
+    }
+}
