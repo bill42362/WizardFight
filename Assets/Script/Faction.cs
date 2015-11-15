711ca739bb59082176c77ebaf054e8bb24a5e293
unknown <>
2015-11-15 22:28:21 +0800

    Faction

--- Assets/Script/Faction.cs
+++ Assets/Script/Faction.cs
@@ -19,7 +19,7 @@ public class Faction : Photon.PunBehaviour {
     public override void OnPhotonInstantiate(PhotonMessageInfo info)
     {
         base.OnPhotonInstantiate(info);
-        if ( !NetworkManager.Instance.isOffline )
+        if ( owner == -1 && !NetworkManager.Instance.isOffline )
         {
             SetFaction(info.sender.ID);
         }
