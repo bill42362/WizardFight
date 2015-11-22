f90b95dde92cd4bccc98892d24bcbe5e17090640
Bill42362 <bill42362@gmail.com>
2015-11-22 17:49:44 +0800

    Bind RoomSkillButton to skill casters.

--- Assets/Script/RoomCanvas/RoomSkillButton.cs
+++ Assets/Script/RoomCanvas/RoomSkillButton.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RoomSkillButton : MonoBehaviour {
 	public int skillIndex = 0;
-	public int skillId = 1;
+	public int playerId = 1;
+	private SkillProperties skillProperties;
+	void Awake() {
+        EventManager.Instance.RegisterListener(EventManager.Instance, "playerSkillsReady", gameObject, OnPlayerSkillsReady);
+	}
+    public void OnPlayerSkillsReady(SbiEvent e) {
+        PlayerSkillsReadyEventData data = (PlayerSkillsReadyEventData)e.data;
+		Role role = (Role)data.player.GetComponent<Role>();
+		if(playerId != role.playerId) { return; }
+		GameObject[] playerSkillCasters = data.skillCasters;
+		if(playerSkillCasters.Length > skillIndex) {
+			skillProperties = playerSkillCasters[skillIndex].GetComponent<SkillProperties>();
+		}
+		if(null != skillProperties) {
+			GetComponentInChildren<Text>().text = skillProperties.skillName;
+			name = skillProperties.skillName + " Button";
+			GetComponent<Image>().color = skillProperties.buttonColor;
+		}
+    }
 }
--- Assets/Script/RoomCanvas/RoomSkillsSpan.cs
+++ Assets/Script/RoomCanvas/RoomSkillsSpan.cs
@@ -3,4 +3,10 @@ using System.Collections;
 
 public class RoomSkillsSpan : MonoBehaviour {
 	public int playerId = 1;
+	void Awake() {
+		RoomSkillButton[] buttons = GetComponentsInChildren<RoomSkillButton>();
+		foreach(RoomSkillButton button in buttons) {
+			button.playerId = playerId;
+		}
+	}
 }
