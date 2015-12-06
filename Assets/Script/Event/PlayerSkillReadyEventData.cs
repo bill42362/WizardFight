6c3908c071c3922c2128d929f23a60caeeff3461
Bill42362 <bill42362@gmail.com>
2015-12-06 20:51:01 +0800

    Refactor GameManager.

---
+++ Assets/Script/Event/PlayerSkillReadyEventData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSkillReadyEventData : SbiEventData {
+    public GameObject player;
+    public int skillIndex;
+    public GameObject skillCaster;
+    public PlayerSkillsReadyEventData(GameObject p, int index, GameObject caster) {
+        player = p;
+        skillIndex = index;
+        skillCaster = caster;
+    }
+}
--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using SkillCasterDictionary = Dictionary< int , GameObject >;
 public class GameManager : MonoBehaviour {
     //  ***** Singleton Related *****
     private static GameManager _instance = null;
-    protected GameManager() {
-    }
+    protected GameManager() { }
     public static GameManager Instance
     {
         get
@@ -25,130 +24,93 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-
     // ***** Overall Game Constant *****
-    private static string gameVersion = "0.00001";
-    public string GetGameVersion() { return gameVersion; }
-
+    public static string gameVersion = "0.00001";
 
     // ***** Player Information *****
     public string playerName = "username";
-    public int playerID = 1;
+    public int PlayerId { get { return NetworkManager.Instance.PlayerID; } }
+    public int[] playerSkillIds = {0, 1, 2};
     private GameObject mainCamera = null;
 
     // ***** Character Information *****
-    private int max_player = 2; // FIXME , this should depend on map.
-    private Dictionary<int, int> characterOrder;
-    private Dictionary<int, GameObject> characters;
-    private Dictionary<int, GameObject[]> characterSkillCasters;
-    private Dictionary<int, int[]> characterSkillIDs;
-
+    public int maxPlayer = 2; // FIXME , this should depend on map.
+    private Dictionary<int, int> characterOrder = new Dictionary<int, int>();
+    private Dictionary<int, GameObject> characters = new Dictionary<int, GameObject>();
+    private Dictionary<int, SkillCasterDictionary> characterSkillCasters
+		= new Dictionary<int, SkillCasterDictionary>();
 
     // ***** Character Public Methods *****
-    public void InitializeGame()
-    {
-        characterOrder = new Dictionary<int, int>();
-        characters = new Dictionary<int, GameObject>();
-        characterSkillCasters = new Dictionary<int, GameObject[]>();
-        characterSkillIDs = new Dictionary<int, int[]>();
-
-        SetPlayerID(NetworkManager.Instance.GetPlayerID());
-        SetCharacterSkillIDs(GetPlayerID(), new int[3] { 0, 1, 2 });
-        
-    }
-    public void SetOrder( int ID , int order)
-    {
-        characterOrder[ID] = order;
-    }
-    public int GetOrder( int ID)
-    {
-        return characterOrder[ID];
-    }
-
-    public void SetPlayerID(int ID) { playerID = ID; }
-    public int GetPlayerID() { return playerID; }
-
-    public void SetCharacter(int ID, GameObject me) { characters[ID] = me; }
-    public GameObject GetCharacter(int ID = -1)
-    {
-        if (ID == -1) ID = playerID;
-        return characters[ID];
-    }
-    public GameObject GetPlayerCharacter() { return GetCharacter(); }
-    public void SetCharacterSkillIDs(int ID, int[] skillIDs)
-    {
-        characterSkillIDs[ID] = skillIDs;
-        GameObject[] skillCasters = new GameObject[skillIDs.Length];
-        for (int i = 0; i < skillIDs.Length; ++i)
-        {
-            skillCasters[i] = DataManager.Instance.createSkillCasterByID(skillIDs[i]);
-        }
-        characterSkillCasters[ID] = skillCasters;
-    }
-    public int[] GetCharacterSkillIDs(int ID = -1) {
-        if (ID == -1)
-            return characterSkillIDs[playerID];
-        return characterSkillIDs[ID];
-    }
-    public GameObject[] GetCharacterSkillCasters(int ID)
-    {
-        return (GameObject[])characterSkillCasters[ID];
-    }
-
-    // FIXME: It should be GetName(ID) 
-    public string GetPlayerName() { return Instance.playerName; }
-
+    public void InitializeGame() { }
+    public void SetOrder(int ID , int order) { characterOrder[ID] = order; }
+    public void SetCharacter(int playerId, GameObject character) {
+		characters[playerId] = character;
+		PlayerChangeEventData data = new PlayerChangeEventData(character);
+		EventManager.Instance.CastEvent(this, "characterReady", data);
+		if(IsCharactersAndCastersReady()) { OnCharactersAndCastersReady(); }
+	}
+    public void SetSkillCaster(int playerId, int skillIndex, GameObject skillCaster) {
+		if(null == characterSkillCasters[playerId]) {
+			characterSkillCasters[playerId] = new Dictionary<int, GameObject>();
+		}
+		characterSkillCasters[playerId][skillIndex] = skillCaster;
+		PlayerSkillReadyEventData data = new PlayerSkillReadyEventData(
+			characters[playerId], skillIndex, skillCaster
+		);
+		EventManager.Instance.CastEvent(this, "casterReady", data);
+		if(IsCharactersAndCastersReady()) { OnCharactersAndCastersReady(); }
+	}
+	private bool IsCharactersAndCastersReady() {
+		if(2 != characters.Keys.Count) { return false; }
+		if(2 != characterSkillCasters.Keys.Count) { return false; }
+		foreach(KeyValuePair<int, SkillCasterDictionary> casters in characterSkillCasters) {
+			if(5 != casters.Keys.Count) { return false; }
+		}
+		return true;
+	}
+	private void OnCharactersAndCastersReady() {
+		NetworkManager.Instance.Ready();
+	}
 
     // ***** NetworkManager Coupling methods *****
-    private void SetGameProperties()
-    {
-        ExitGames.Client.Photon.Hashtable playerProps = new ExitGames.Client.Photon.Hashtable();
-        playerProps.Add("name", playerName);
-        playerProps.Add("order", characterOrder[playerID]);
-        playerProps.Add("skillIds", characterSkillIDs[playerID]);
-        NetworkManager.Instance.SetPlayerProperties(playerProps);
-    }
-
     public void OnLeftRoom() { mainCamera.SetActive(true); }
     public void OnPlayerJoinRoom(int playerOrder) {
-        SetOrder(playerID, playerOrder);
-        GameObject playerCharacter = InstantiateCharacter(playerID,playerOrder);
+        SetOrder(PlayerId, playerOrder);
+        GameObject playerCharacter = InstantiateCharacter(playerID, playerOrder);
         Camera camera = playerCharacter.GetComponentsInChildren<Camera>(true)[0];
         camera.gameObject.SetActive(true);
         mainCamera = GameObject.FindWithTag("MainCamera");
         mainCamera.SetActive(false);
        
-        SetCharacter(playerID, playerCharacter);         // Player Character must be set at first side
-
-
-        if (NetworkManager.Instance.isOffline) {
+        if(NetworkManager.Instance.isOffline) {
             GameObject neutralCharacter = InstantiateCharacter(0, 2);
             SetOrder(0, 2);
-            //SetCharacterSkillIDs(0, new int[] { 0, 1, 2 });
-           
         }
-        SetGameProperties();
-    }
 
+		for(int i = 0; i < playerSkillIds.Length; ++i) {
+			NetworkManager.Instance.Instantiate(
+				DataManager.Instance.GetSkillCasterPrefabString(playerSkillIds[i]),
+				new Vector3(0, 0, positionZ),
+				Quaternion.identity,
+				0,
+				CreateCasterInsiantiateData(ID, i)
+			);
+		}
+    }
+    public void OnOtherPlayerJoinRoom(int id, int order) { SetOrder(id, order); }
 
     // ***** Game Logic Public Methods *****
     public GameObject InstantiateCharacter(int ID, int order) {
         float positionZ = (order == 1) ? -5 : 5;
-        GameObject character = NetworkManager.Instance.Instantiate("unitychan",
-                                                                   new Vector3(0, 0, positionZ),
-                                                                   Quaternion.identity,
-                                                                   0,
-                                                                   CreateInstantiateData(ID));
+        GameObject character = NetworkManager.Instance.Instantiate(
+			"unitychan", new Vector3(0, 0, positionZ), Quaternion.identity, 0, CreatePlayerInsiantiateData(ID)
+		);
         return character;
     }
-
-    // ***** Game Logic Private Utility Methods *****
-    public const int INSTANTIATE_DATA_INDEX_COUNT = 1;
-    public const int INSTANTIATE_DATA_ID = 0;
-    private object[] CreateInstantiateData(int ID)
-    {
-        object[] instantiationData = new object[INSTANTIATE_DATA_INDEX_COUNT];
-        instantiationData[INSTANTIATE_DATA_ID] = (object)ID;
-        return instantiationData;
+    private object[] CreatePlayerInsiantiateData(int playerId) {
+        return new object[] {(object)playerId};
+    }
+    private object[] CreateCasterInsiantiateData(int playerId, int skillIndex) {
+        return new object[] {(object)playerId, (object)skillIndex};
     }
 }
