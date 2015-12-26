9344d9b7c4651d15c5a1526e85dee5d722a3c698
Bill42362 <bill42362@gmail.com>
2015-12-27 01:32:30 +0800

    Fix FireBallBullet lifetime.

--- Assets/Script/Skill/FireBall/FireBallBullet.cs
+++ Assets/Script/Skill/FireBall/FireBallBullet.cs
@@ -4,7 +4,8 @@ public class FireBallBullet : MonoBehaviour {
     public static GameObject CreateInstance( double time , Vector3 pos, Vector3 dir , Faction faction, FireBallCaster caster)
     {
         GameObject obj = (GameObject)GameObject.Instantiate(
-    Resources.Load("Prefab/Skill/FireBallBullet"), pos, Quaternion.LookRotation(dir));
+			Resources.Load("Prefab/Skill/FireBallBullet"), pos, Quaternion.LookRotation(dir)
+		);
         obj.GetComponent<FireBallBullet>().SetCreationParameters(pos, dir, time, caster);
         obj.GetComponent<Faction>().SetFaction(faction);
         return obj;
@@ -20,8 +21,7 @@ public class FireBallBullet : MonoBehaviour {
 	private Faction faction;
     private FireBallCaster caster;
 
-    public void SetCreationParameters(Vector3 pCreatePosition,Vector3 pForward, double pTime, FireBallCaster pCaster)
-    {
+    public void SetCreationParameters(Vector3 pCreatePosition,Vector3 pForward, double pTime, FireBallCaster pCaster) {
         this.createPosition = pCreatePosition;
         this.createTime = pTime;
         this.createForward = pForward;
@@ -36,8 +36,6 @@ public class FireBallBullet : MonoBehaviour {
 		if((createTime + lifeTime) < timestamp) { Destroy(gameObject); }
         float diffTime = (float)(PhotonNetwork.time - createTime);
         transform.position = createPosition + createForward * (float)(flyingSpeed * diffTime);
-
-
 	}
 	public void OnTriggerStay(Collider other) {
 		Role role = other.gameObject.GetComponent<Role>();
