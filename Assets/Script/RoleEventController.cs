84ad99d632872d1b961e32b0842e18d89072db04
simpdanny <simpdanny@gmail.com>
2015-12-27 16:00:29 +0800

    no input delay

--- Assets/Script/RoleEventController.cs
+++ Assets/Script/RoleEventController.cs
@@ -5,8 +5,6 @@ public class RoleEventController : Photon.PunBehaviour {
 	public Dictionary<string, Vector3> eventPairs = new Dictionary<string, Vector3>();
     public bool isControllable = false;
 	private Role role;
-    private bool isLeft;
-    private double startTime = -1;
     private const float speed = 15;
     private const float acceleration = -30; 
     private bool isMoving = false;
@@ -21,7 +19,6 @@ public class RoleEventController : Photon.PunBehaviour {
 		}
 	}
 	public void Update () {
-        UpdateAnimation();
         if ( isControllable ) {
             UpdateVelocity();
         }
@@ -31,39 +28,22 @@ public class RoleEventController : Photon.PunBehaviour {
     {
         GetComponent<PhotonTransformView>().SetSynchronizedValues(GetComponent<Rigidbody>().velocity, 0);
     }
-    private void UpdateAnimation()
-    {
-        if (startTime > 0 && PhotonNetwork.time >= startTime)
-        {
-            string type = (isLeft) ? "leftButtonClick" : "rightButtonClick";
-            Vector3 velocity = transform.localToWorldMatrix.MultiplyVector(eventPairs[type] * speed);
-            GetComponent<Rigidbody>().velocity = velocity;
-            startTime = -1f;
-        }
-    }
 
 	public void OnEventTriggered(SbiEvent e) {
         bool isLeft = (e.type == "leftButtonClick");
         
         if (CanMove())
         {
-            float delay = NetworkManager.Instance.RPCDelay;
-            this.photonView.RPC("StartMove",PhotonTargets.All, (float)PhotonNetwork.time + delay, isLeft);
+            string type = (isLeft) ? "leftButtonClick" : "rightButtonClick";
+            Vector3 velocity = transform.localToWorldMatrix.MultiplyVector(eventPairs[type] * speed);
+            GetComponent<Rigidbody>().velocity = velocity;
         }
     }
 
     public bool CanMove()
     {
-        return startTime < 0 && GetComponent<Rigidbody>().velocity.magnitude < 0.01;
+        return  GetComponent<Rigidbody>().velocity.magnitude < 0.01;
        
     }
-    [PunRPC]
-    public void StartMove( float startTime , bool isLeft)
-    {
-        if (!isControllable) { 
-            NetworkManager.Instance.UpdateRPCDelay((float)PhotonNetwork.time - (float)startTime);
-        }
-        this.startTime = startTime;
-        this.isLeft = isLeft;
-    }
+
 }
